d61195800711eca6940977c319aad33caac8b6ea
Trevor Cickovski <tcickovs@delphi.cs.fiu.edu>
2023-07-28 15:49:43 -0400

    Parsed out EFF

--- CSVPadPlugin.py
+++ CSVPadPlugin.py
@@ -1,10 +1,16 @@
 class CSVPadPlugin:
    def input(self, filename):
       f = open(filename, 'r')
-      self.contents = f.read()
+      self.contents = f.read().strip()
 
    def run(self):
-      self.contents = "\"\"," + self.contents
+      # Parse out FEFF, common in CSVs
+      while (ord(self.contents[0]) == 65279):
+         self.contents = self.contents[1:]
+      if (self.contents[0] != ','):
+         self.contents = "\"\"," + self.contents
+      else:
+         self.contents = "\"\"" + self.contents
 
    def output(self, filename):
       g = open(filename, 'w')
